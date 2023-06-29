14903a12a8b083fd4d5d2ee7f00aa946e3a31440
francitonioferreiraalves <francitonio@gmail.com>
2023-06-29 15:44:13 -0300

    criando segurança

--- src/main/java/com/ferreira/auth/infra/security/SecurityConfigurations.java
+++ src/main/java/com/ferreira/auth/infra/security/SecurityConfigurations.java
@@ -1,4 +1,4 @@
-package com.ferreira.auth.infra.security;
+	package com.ferreira.auth.infra.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
